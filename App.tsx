a6ce08112081e173707c63d145bb7dcc765e7eb7
michael <stevenpnd@gmail.com>
2021-11-20 13:08:16 -0500

    Set up auth

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider} from 'native-base';
 import { createStackNavigator} from '@react-navigation/stack';
 import { Home } from "./src/screens/Home";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Auth } from "./src/screens/auth/Auth";
 
 const Stack = createStackNavigator();
 
-const MyStack = () => {    
+const MyStack = () => {
     return (
         <Stack.Navigator initialRouteName="Home">
              <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
@@ -17,11 +19,15 @@ const MyStack = () => {
 const App = () => {
 
     return (
-        <NativeBaseProvider>
-            <NavigationContainer>
-                <MyStack />
-            </NavigationContainer>
-        </NativeBaseProvider>);
+        // <NativeBaseProvider>
+        //     <NavigationContainer>
+        //         <MyStack />
+        //     </NavigationContainer>
+        // </NativeBaseProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+        <Auth />
+        </SafeAreaView>
+    );
 };
 
 
---
+++ src/screens/auth/Profile.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+
+export default function Profile() {
+    const user = auth().currentUser;
+
+    async function signOut() {
+        try {
+            await GoogleSignin.revokeAccess();
+            auth().signOut();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>You're Logged In</Text>
+            <Image source={{ uri: user?.photoURL || "" }} style={styles.image} />
+            <Text style={styles.text}>{user?.displayName}</Text>
+            <Text style={styles.text}>{user?.email}</Text>
+            <View style={{ marginTop: 30 }}>
+                <Button title="Signout" onPress={signOut} />
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 25,
+        marginBottom: 30,
+    },
+    image: {
+        height: 150,
+        width: 150,
+        borderRadius: 150,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 20,
+    },
+});
