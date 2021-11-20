c1b871d6d83a9a19e0a27eb9b35ddeeb01a33f4a
michael <stevenpnd@gmail.com>
2021-11-19 23:20:27 -0500

    Set up stack

---
+++ src/components/ScrollCategories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ScrollCategories = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/resources/Constants.tsx
@@ -0,0 +1,12 @@
+const COLORS = {
+    YELLOW: "#F4D73B",
+    BLUE: "#0069A3",
+    LIGHT_GRAY: "#BEBEBE",
+    DARK_WHITE: "#DDDDDD",
+    GRAY: "#929292",
+    BLACK: "#000000",
+    WHITE: "#FFFFFF"
+}
+
+
+export {COLORS};
